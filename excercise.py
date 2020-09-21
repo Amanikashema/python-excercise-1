6c897d743a1f8e03a55a13a84cb242ea8fe6f214
Your Name <amanikashema000@gmail.com>
2020-09-21 11:56:32 +0200

    2 excercise

---
+++ excercise.py
@@ -0,0 +1,14 @@
+first_name = input("Please enter your first name:")
+second_name = input("Please enter your second name:")
+age = input("how old are you:")
+full_name = first_name + " " + second_name
+email_address = first_name + second_name + "@gmail.com"
+print("Hello " + full_name + " You are " + age + " Old and you email address is " + email_address)
+
+
+#calculation of net pay for emploiyees
+
+income_tax = 0.15
+salary = int(input("Please  enter your salary: "))
+net_pay = salary - (salary*income_tax)
+print("Your total net pay =" , net_pay)
